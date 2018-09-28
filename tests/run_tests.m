f323ab80f05874046af58f0173f49c7697b1cefc
david <davidnsousa@gmail.com>
2018-09-28 18:42:21 +0100

    fixed issues #10, #11 and #14

--- tests/run_tests.m
+++
@@ -1,8 +0,0 @@
-addpath("~/git/mcsd/mcsd"); 
-addpath("~/git/mcsd/tests");
-test_free_diffusion1D
-test_free_diffusion2D
-test_free_diffusion3D
-test_isotropic_restricted_diffusion1D
-test_isotropic_restricted_diffusion2D
-test_anisotropic_restricted_diffusion3D
